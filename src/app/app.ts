d6ddda8a92e3e59a99c1eef1363c423dde86a872
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2026-01-19 18:52:35 +0100

    feat: About Me section ready

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { AboutMe } from './components/about-me/about-me';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, AboutMe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
